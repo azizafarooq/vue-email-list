4138e8997c5a886e662b917b09bb77fe4fe9e6fe
azizafarooq <azizafarooq2000@gmail.com>
2024-02-06 16:14:26 +0100

    feat: js vue

--- js/main.js
+++ js/main.js
@@ -1,8 +1,18 @@
-for (i=0; i<10; i++){
-    axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-    .then((paperino =>{
-        console.log(paperino.data)
-    } ))
-}
-
-    
+const { createApp } = Vue;
+const app = createApp({
+    data() {
+      return {
+        emails:[],
+      }
+    },
+    created(){
+        for (i=0; i<10; i++){
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((paperino =>{
+                this.emails.push(paperino.data);
+            } ))
+        }
+    }
+});
+  
+app.mount('#app');
