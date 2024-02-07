de9e7ac2407e538723a44b98a729dc2899f1172a
azizafarooq <azizafarooq2000@gmail.com>
2024-02-07 10:05:07 +0100

    fix: js

--- js/main.js
+++ js/main.js
@@ -4,10 +4,11 @@ const app = createApp({
       return {
         title: "Random Emails",
         emails:[],
+        requiredEmails: 10,
       }
     },
     created(){
-        for (i=0; i<10; i++){
+        for (i=0; i<this.requiredEmails; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((paperino =>{
                 this.emails.push(paperino.data.response);
